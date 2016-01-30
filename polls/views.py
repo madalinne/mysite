abc0d28c4885e5a0036c58be2a2114508211d53a
madalina <madalina.te.rijdt@gmail.com>
2016-01-31 00:49:21 +0100

    simple form - correct mistakes sintax

--- polls/views.py
+++ polls/views.py
@@ -30,7 +30,7 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     try:
-        selected_choice = question.chocie_set.get(pk = request.POST['choice'])
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
          # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
