78798015cdce613638f57ed6f4752d309d433519
madalina <madalina.te.rijdt@gmail.com>
2016-01-30 23:47:04 +0100

    views settings

--- mysite/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def post_list(request):
-    return render(request, 'mysite/post_list.html', {})
--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,13 @@ from . import views
 
 urlpatterns = [
     #url(r'^$', views.index, name='index'),
-    url(r'^$', views.post_list, name='post_list'),
+   # url(r'^$', views.post_list, name='post_list'),
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
