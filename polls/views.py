e5b0833af5cca23291e2b7913bec6cd3f59e71a6
madalina <madalina.te.rijdt@gmail.com>
2016-01-30 23:42:34 +0100

    add views.py to mysite folder

--- polls/views.py
+++ polls/views.py
@@ -5,9 +5,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def post_list(request):
-    return render(request, 'mysite/post_list.html', {})
-
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
