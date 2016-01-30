cb6c6e7a27a8bda5d3ee9c73f86c80b15cc61879
madalina <madalina.te.rijdt@gmail.com>
2016-01-31 00:11:50 +0100

    view - detail + template - correct compile problem

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import rende
-from django.template import loaderr
+from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 
